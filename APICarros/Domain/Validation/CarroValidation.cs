2098b9a7d797b43acf70a52996b907e458a3fba8
Deassis Barbosa <diassisbarbosa.db@gmail.com>
2025-10-12 22:12:28 -0300

    Ano de Fabricação
    Correção da mensagem de fabricação do carro.

--- APICarros/Domain/Validation/CarroValidation.cs
+++ APICarros/Domain/Validation/CarroValidation.cs
@@ -6,7 +6,7 @@ namespace APICarros.Domain.Validation
         public CarroValidation()
         {
             RuleFor(car => car.Modelo).NotEmpty().WithMessage("O modelo do carro é obrigatório.");
-            RuleFor(car => car.Ano).GreaterThan(1900).WithMessage("Ano de Fabricação deve ser maior que 1990.");
+            RuleFor(car => car.Ano).GreaterThan(1900).WithMessage("Ano de Fabricação deve ser maior que 1900.");
             RuleFor(car => car.Cor).NotEmpty().WithMessage("A cor do carro é obrigatória.");
         }
     }
